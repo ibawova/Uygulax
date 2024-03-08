198b1f1b72d720b0d2205e00cbdbf41ff299b322
ibawova <159722417+ibawova@users.noreply.github.com>
2024-03-08 16:07:05 +0300

    Delete gthn/addons directory

--- gthn/addons/plugin.video.stubevavoo/lib/video1.py
+++
@@ -1,4 +0,0 @@
-import base64, codecs
-stube = 'aW1wb3J0IHZpZGVvDQp2aWRlby5tYWluKCk='
-trust = eval('stube')
-eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
--- gthn/addons/plugin.video.tvnow.de/sendung.py
+++
@@ -1,6 +0,0 @@
-class Sendung():
-    def __init__(self, sid, title, station, tGroup):
-        self.sid = sid
-        self.title = title
-        self.station = station
-        self.tGroup = tGroup
